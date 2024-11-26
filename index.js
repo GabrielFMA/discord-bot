a004994c71abeff50286df1e026da6be71cefc45
Letícia1300 <shizukaregame20@gmail.com>
2024-11-25 22:31:26 -0300

    mudei os names,coloquei imagens.

--- index.js
+++ index.js
@@ -37,14 +37,15 @@ client.on('interactionCreate', async (interaction) => {
     if (interaction.commandName === 'ping') {
 
         const pingEmbed = new EmbedBuilder()
-            .setColor('#088A08')
-            .setTitle('Pamonhas!')
+            .setColor('#33f1ff')
+            .setTitle('Ressonadors')
             .addFields(
-                { name: 'Letícia', value: 'Conhecido mais como Shizu. <:Black_Shores_Emblem:1309710724284551178>', inline: true },
-                { name: 'Gabriel', value: 'Conhecido mais como Anayki. <:Huanglong_Emblem:1309707973060333669>', inline: true },
-                { name: 'Localização', value: 'Brasil', inline: true }
+                { name: 'Ressonadora', value: 'Shorekepp. <:emoji_7:1310775782775324692> <:emoji_2:1310771570909909143>', inline: true },
+                { name: 'Raridade', value: '<:imagem_20241125_222238287:1310778083912978484', inline: true },
+                { name: 'Localização', value: '<:Black Shores. <:Black_Shores_Emblem:1310776445592797284>', inline: true },
+                
             )
-            .setThumbnail('https://cdn.discordapp.com/attachments/1273496005484216390/1310758140953759744/Black_Shores_Emblem.webp?ex=67466225&is=674510a5&hm=542589e6591749b78002084275e1c074df613f12fc64509101ba2afac8365a0b&') // Imagem pequena à direita
+            .setThumbnail('https://media.discordapp.net/attachments/1273496005484216390/1310778898316791828/1000.png?ex=6746757a&is=674523fa&hm=ff9b44388b55734087b0f72ea48ece6712af084b808c30c641e69149f8c352fb&=&format=webp&quality=lossless&width=550&height=236') // Imagem pequena à direita
             .setImage('https://cdn.discordapp.com/attachments/1273496005484216390/1310758140953759744/Black_Shores_Emblem.webp?ex=67466225&is=674510a5&hm=542589e6591749b78002084275e1c074df613f12fc64509101ba2afac8365a0b&') // Imagem grande na parte inferior
             .setFooter({
                 text: 'Informações sobre o bot',
